29f83f0912553e297f6a12b34a5a06623b650a39
edenmer <edenmer5@gmail.com>
2021-12-14 22:28:41 +0200

    update

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ int toInt(char str){
 
 void minSequenceGematria(char[] str, int size){
     int i,j,k,sum=0;
-    for(i=0; i<sizeof(str); i++){
-        for(j=i; j<sizeof(str); j++){
+    for(i=0; i<(sizeof(str)/sizeof(str[0])); i++){
+        for(j=i; j<(sizeof(str)/sizeof(str[0])); j++){
             sum=sum+toInt(str[j]);
             if(sum>size) 
                 break;
@@ -31,9 +31,9 @@ void minSequenceGematria(char[] str, int size){
     }
 }
 
-void gematria(word, sentence){
+void gematria(char[] word, char[] sentence){
     int word_size =0;
-    for (int i = 0; i < sizeof(word); i++){
+    for (int i = 0; i < sizeof(word)/sizeof(word[0]); i++){
        word_size += toInt(word[i]);
     }
     printf("Gematria Sequences: ");
@@ -42,11 +42,57 @@ void gematria(word, sentence){
 }
 
 
-char toAtbash(char c){
+int toAtbash(char c){
   if(c<90)
         return 90-(c-'A');
     else
         return 122-(c-'a');    
+}
+
+void minSequenceAtbash(char[] sentence, char[] word){
+    int i,j,k;
+    for(i=0; i<(sizeof(sentence)/sizeof(sentence[0]));i++){
+        k=0;
+        for (j = i; j < (sizeof(sentence)/sizeof(sentence[0])); j++){
+            if (sentence[j]==word[k]){
+                     k++;                
+                if (k==sizeof(word)/sizeof(word[0])){
+                    for (k=i; k<j; k++)
+                       printf(sentence[k]);
+                    printf("~");
+                    break;   
+                }
+            }else{
+                if(sentence[j] == ' '){
+                    if(i==j)
+                        break;
+                    else
+                        continue;
+                }else
+                    break;
+            }
+        }
+        k=sizeof(word)/sizeof(word[0]);
+        for (j = i; j < (sizeof(sentence)/sizeof(sentence[0])); j++){
+            if (sentence[j]==word[k]){
+                     k--;                
+                if (k==0){
+                    for (k=i; k<j; k++)
+                       printf(sentence[k]);
+                    printf("~");
+                    break;   
+                }
+            }else{
+                if(sentence[j] == ' '){
+                    if(i==j)
+                        break;
+                    else
+                        continue;
+                }else
+                    break;
+            }
+        }
+    }
 }
 
 
